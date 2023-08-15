9d601cee37fe9cb0f2a259283305e51990efc48d
mhap75 <mhap75@yahoo.com>
2023-08-15 15:14:03 +0330

    Initial setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+const App = () => (
+  <div className="bg-primary w-full overflow-hidden">
+    {/* Navbar.jsx */}
+    <div className="paddingX flexCenter">
+      <div className="boxWidth">
+        Navbar
+      </div>
+    </div>
+  </div>
+)
+
+export default App
