9fbf326f35d6926ab8f8fcc38b13edac4d567d4f
Mohammad Hossein Amirpanahi <118535877+mhap75@users.noreply.github.com>
2023-08-17 15:35:51 +0330

    Develop CTA

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,9 +1,21 @@
+import { Button } from "./";
+
 const CTA = () => {
-  return (
-    <div>
-      Enter
-    </div>
-  );
-}
+	return (
+		<section className="flexCenter marginY padding flex-col sm:flex-row bg-black-gradient-2 rounded-[20px] box-shadow">
+			<div className="flex-1 flex flex-col">
+				<h2 className="heading2">Let&apos;s try our service now!</h2>
+				<p className="paragraph max-w-[470px] mt-5">
+					All the essential tools expanding your business globally,
+					accessible from anywhere on the planet.
+				</p>
+			</div>
+
+			<div className="flexCenter sm:ml-10 ml-0 sm:mt-0 mt-10">
+				<Button>Get Started</Button>
+			</div>
+		</section>
+	);
+};
 
 export default CTA;
